eb7de37a82e4476a3f12281c93ad2d0a47f973b5
Joseph Sawyer <joseph@jcsawyer.com>
2020-07-26 16:33:06 +0100

    MultiTenancyBuilder azure blob storage extensions

---
+++ src/Jc.MultiTenancy.Azure/MultiTenancyBuilderExtensions.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+using Jc.MultiTenancy;
+using Jc.MultiTenancy.Azure;
+
+namespace Microsoft.Extensions.DependencyInjection
+{
+    /// <summary>
+    /// <see cref="MultiTenancyBuilder"/> azure extensions
+    /// </summary>
+    public static class MultiTenancyBuilderExtensions
+    {
+        /// <summary>
+        /// Adds the azure blob store for tennts
+        /// </summary>
+        /// <param name="optionsAction"><see cref="Action{BlobTenantStoreOptions}"/> options action</param>
+        /// <returns></returns>
+        public static MultiTenancyBuilder AddAzureBlobStore(
+            this MultiTenancyBuilder builder,
+            Action<BlobTenantStoreOptions> optionsAction = null)
+        {
+            AddStores(builder.Services, builder.TenantType, optionsAction);
+            return builder;
+        }
+
+        /// <summary>
+        /// Adds the store services for <paramref name="tenantType"/> with
+        /// the specified <paramref name="optionsAction"/> to the <paramref name="services"/>
+        /// collection
+        /// </summary>
+        /// <param name="services"><see cref="IServiceCollection"/> services</param>
+        /// <param name="tenantType"><see cref="Type"/> of tenant</param>
+        /// <param name="optionsAction"><see cref="Action{BlobTenantStoreOptions}"/> options action</param>
+        private static void AddStores(
+            [NotNull] IServiceCollection services,
+            [NotNull] Type tenantType,
+            Action<BlobTenantStoreOptions> optionsAction)
+        {
+            Type tenantStoreType = typeof(TenantStore<>).MakeGenericType(tenantType);
+
+            var options = new BlobTenantStoreOptions();
+            optionsAction?.Invoke(options);
+
+            services.TryAddScoped(services => options);
+            services.TryAddScoped(typeof(ITenantStore<>).MakeGenericType(tenantType), tenantStoreType);
+        }
+    }
+}
