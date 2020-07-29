3b834b2280220b6d2f952173d7c8052438bdd9c0
Joseph Sawyer <joseph@jcsawyer.com>
2020-07-29 19:35:08 +0100

    Jc.MultiTenancy.Stores tests

---
+++ test/Jc.MultiTenancy/Jc.MultiTenancy.Stores.Tests/Mocks/MockTenantStoreBase.cs
@@ -0,0 +1,45 @@
+﻿using Jc.Core;
+using System;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jc.MultiTenancy.Stores.Tests.Mocks
+{
+    public class MockTenantStoreBase : TenantStoreBase<Tenant>
+    {
+        public override IQueryable<Tenant> Tenants => throw new NotImplementedException();
+
+        public override Task<JcResult> CreateAsync(Tenant tenant, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<JcResult> UpdateAsync(Tenant tenant, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<JcResult> DeleteAsync(Tenant tenant, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<Tenant> FindByHostAsync(string host, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<Tenant> FindByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override Task<Tenant> FindByNameAsync(string name, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+
+        
+    }
+}
---
+++ test/Jc.MultiTenancy/Jc.MultiTenancy.Stores.Tests/TenantStoreBaseTests.cs
@@ -0,0 +1,85 @@
+﻿using Jc.MultiTenancy.Stores.Tests.Mocks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Jc.MultiTenancy.Stores.Tests
+{
+    [TestClass]
+    public class TenantStoreBaseTests
+    {
+        private readonly MockTenantStoreBase _sut;
+
+        public TenantStoreBaseTests()
+        {
+            _sut = new MockTenantStoreBase();
+        }
+
+        [TestMethod]
+        public async Task SetNameAsync_Sets_Name()
+        {
+            var tenant = new Tenant();
+            string expected = "Tenant Name";
+
+            await _sut.SetNameAsync(tenant, expected);
+
+            Assert.AreEqual(expected, tenant.Name);
+        }
+
+        [TestMethod]
+        public async Task SetNameAsync_Throws_When_Cancellation_Requested()
+        {
+            var tenant = new Tenant();
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.Cancel();
+
+            await Assert.ThrowsExceptionAsync<OperationCanceledException>(async () => await _sut.SetNameAsync(tenant, "Tenant Name", cancellationTokenSource.Token));
+        }
+
+        [TestMethod]
+        public async Task SetNameAsync_Throws_When_Disposed()
+        {
+            var disposableMock = new MockTenantStoreBase();
+            var tenant = new Tenant();
+
+            disposableMock.Dispose();
+
+            await Assert.ThrowsExceptionAsync<ObjectDisposedException>(async () => await disposableMock.SetNameAsync(tenant, "Tenant Name"));
+        }
+
+        [TestMethod]
+        public async Task SetHostAsync_Sets_Host()
+        {
+            var tenant = new Tenant();
+            string expected = "tenant.host";
+
+            await _sut.SetHostAsync(tenant, expected);
+
+            Assert.AreEqual(expected, tenant.Host);
+        }
+
+        [TestMethod]
+        public async Task SetHostAsync_Throws_When_Cancellation_Requested()
+        {
+            var tenant = new Tenant();
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            cancellationTokenSource.Cancel();
+
+            await Assert.ThrowsExceptionAsync<OperationCanceledException>(async () => await _sut.SetHostAsync(tenant, "tenant.host", cancellationTokenSource.Token));
+        }
+
+        [TestMethod]
+        public async Task SetHostAsync_Throws_When_Disposed()
+        {
+            var disposableMock = new MockTenantStoreBase();
+            var tenant = new Tenant();
+
+            disposableMock.Dispose();
+
+            await Assert.ThrowsExceptionAsync<ObjectDisposedException>(async () => await disposableMock.SetHostAsync(tenant, "tenant.host"));
+        }
+    }
+}
